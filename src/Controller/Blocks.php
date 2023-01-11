22d8f2a7a1c76653f2a985fc71e7e35aef6d9a64
Agung Sundoro <muhagung2001@gmail.com>
2023-01-11 16:02:28 +0700

    fix: block integration with webpack

--- src/Controller/Blocks.php
+++ src/Controller/Blocks.php
@@ -8,51 +8,60 @@
 
 namespace LayarTancap\Controller;
 
-! defined( 'WPINC ' ) or die;
+!defined('WPINC ') or die;
 
 use LayarTancap\Controller;
+use LayarTancap\WordPress\Hook\Action;
 
-class Blocks extends Controller {
+class Blocks extends Controller
+{
 
 	/**
 	 * Admin constructor
 	 *
 	 * @return void
-	 * @var    object   $plugin     Plugin configuration
+	 * @var    object $plugin Plugin configuration
 	 * @pattern prototype
 	 */
-	public function __construct( $plugin ) {
-		parent::__construct( $plugin );
+	public function __construct($plugin)
+	{
+		parent::__construct($plugin);
 
 		/** @backend - Add custom admin page under settings */
 		$action = new Action();
-		$action->setComponent( $this );
-		$action->setHook( 'init' );
-		$action->setCallback( 'register_block' );
-		$action->setMandatory( true );
+		$action->setComponent($this);
+		$action->setHook('init');
+		$action->setCallback('register_block');
+		$action->setMandatory(true);
 		$this->hooks[] = $action;
 	}
 
 	/**
 	 * Register WP Blocks
 	 */
-	public function register_block(){
+	public function register_block()
+	{
+		$path = $this->Framework->getPath();
+		$blocks = file_get_contents($path['framework_path'] . '/blocks/blocks.json');
+		$blocks = json_decode($blocks, true);
 
+		/** Register Blocks */
+		foreach ($blocks as $block) {
+			/** Register Script */
+			wp_register_script(
+				sprintf("block-%s-js", $block['name']),
+				sprintf("%s/assets/build/js/blocks/%s/%s.min.js", $path['framework_url'], $block['name'], $block['name']),
+				array('wp-blocks'),
+			);
 
-//		/** Register Script */
-//		wp_register_script(
-//			'block-hello-world-js',
-//			HOGB_BLOCKS_URL . '/assets/build/blocks/hello-world.js',
-//			array( 'wp-blocks'),
-//		);
-//
-//		/** Register Block */
-//		register_block_type(
-//			'hands-on-gutenberg-block/hello-world',
-//			array(
-//				'editor_script' => 'block-hello-world-js',
-//			)
-//		);
+			/** Register Block */
+			register_block_type(
+				sprintf("layar-tancap-block/%s", $block['name']),
+				array(
+					'editor_script' => sprintf("block-%s-js", $block['name']),
+				)
+			);
+		}
 	}
 
 }
